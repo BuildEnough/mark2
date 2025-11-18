9ed92938c75547d675488af668ae8758e0234f7d
kbw <rhtmaehcl878@naver.com>
2025-11-18 17:24:15 +0900

    thymeleaf 테스트

--- webMark2/src/main/java/com/buildenough/webmark2/HelloController.java
+++ webMark2/src/main/java/com/buildenough/webmark2/HelloController.java
@@ -1,6 +1,7 @@
 package com.buildenough.webmark2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 
     @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello webMark2";
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello webMark2");
+        return "hello";
     }
 }
