bf6b5f5673da9e8ed29bf6b4f7c2b286f7e0b528
kbw <rhtmaehcl878@naver.com>
2025-11-18 17:09:52 +0900

    webMark2 프로젝트 생성

---
+++ webMark2/src/main/java/com/buildenough/webmark2/WebMark2Application.java
@@ -0,0 +1,13 @@
+package com.buildenough.webmark2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMark2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMark2Application.class, args);
+    }
+
+}
---
+++ webMark2/src/test/java/com/buildenough/webmark2/WebMark2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildenough.webmark2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMark2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
