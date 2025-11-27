f0a84f17ed3dfee9584fa4b3ab8b33bdbcbf3213
kbw <rhtmaehcl878@naver.com>
2025-11-27 13:51:43 +0900

    controller

---
+++ logistics-management/src/main/java/com/buildenough/logisticsmanagement/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.buildenough.logisticsmanagement.controller;
+
+import com.buildenough.logisticsmanagement.dto.ProductCreateRequest;
+import com.buildenough.logisticsmanagement.dto.ProductResponse;
+import com.buildenough.logisticsmanagement.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    // 상품 등록
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct (
+            @Valid @RequestBody ProductCreateRequest request
+    ) {
+        ProductResponse response = productService.createProduct(request);
+        return ResponseEntity.ok(response);
+    }
+
+    // 상품 조회
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+        List<ProductResponse> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+}
