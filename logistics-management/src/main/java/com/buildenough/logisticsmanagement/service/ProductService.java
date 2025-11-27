8b08ed7b4c7c06c2609fab0d09f44b6e315db662
kbw <rhtmaehcl878@naver.com>
2025-11-27 13:31:26 +0900

    service

---
+++ logistics-management/src/main/java/com/buildenough/logisticsmanagement/service/ProductService.java
@@ -0,0 +1,49 @@
+package com.buildenough.logisticsmanagement.service;
+
+import com.buildenough.logisticsmanagement.domain.Product;
+import com.buildenough.logisticsmanagement.domain.ProductStatus;
+import com.buildenough.logisticsmanagement.dto.ProductCreateRequest;
+import com.buildenough.logisticsmanagement.dto.ProductResponse;
+import com.buildenough.logisticsmanagement.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    // 상품 등록
+    @Transactional // 예외 터지면 롤백
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        if (productRepository.existsByCode(request.getCode())) {
+            throw new IllegalArgumentException(("이미 사용 중인 상품 코드입니다: " + request.getCode()));
+        }
+
+        // ProductCreateRequest, 즉 dto를 entity로 변환
+        Product product = new Product(
+                request.getCode(),
+                request.getName(),
+                request.getUnit(),
+                ProductStatus.ACTIVE
+        );
+
+        Product saved = productRepository.save(product);
+        return new ProductResponse(saved);
+    }
+
+    // 상품 전체 조회
+    @Transactional(readOnly = true) // 조회 최적화
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(ProductResponse::new)
+                .toList();
+    }
+}
